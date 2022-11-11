0dcc876366b1421cb3981e0e24e2c8045e260535
Simeon Wright <simeonwright@Simeons-Air.lan>
2022-11-11 14:19:55 -0500

    personal login for conditional

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Simeon21:DeaconWright21@cluster0.btv9pwj.mongodb.net/Personallogin1?retryWrites=true&w=majority', 
+    'dbName': 'Personallogin1'
+};
